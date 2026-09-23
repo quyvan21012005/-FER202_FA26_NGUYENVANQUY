be0512c96b95748b88ae1cdc7c58cca08e5b528f
quyvan21012005 <quydaicaquadi@gmail.com>
2026-09-23 10:46:17 +0700

    update

--- Slot5/baitap-es6-lab2/src/components/AppButton.jsx
+++
@@ -1,9 +0,0 @@
-import Button from 'react-bootstrap/Button';
-
-const AppButton = ({ variant = 'primary', children, ...rest }) => (
-  <Button variant={variant} {...rest}>
-    {children}
-  </Button>
-);
-
-export default AppButton;
